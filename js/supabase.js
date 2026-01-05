66263d342cd431de139ed59d74887d287c6d077e
kayetavi <avijitkayet97@gmail.com>
2026-01-05 23:26:01 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,3 @@
-<script type="module">
 import { createClient } from
 "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
@@ -6,4 +5,3 @@ export const supabase = createClient(
   "https://hbeunczctgzsvxuhdehb.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhiZXVuY3pjdGd6c3Z4dWhkZWhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2Mjk1MzgsImV4cCI6MjA4MzIwNTUzOH0.UpWI8Arpmt5ouf8MAyCTOltKUTMvLRkAYJTukdXGUBU"
 );
-</script>
