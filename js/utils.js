f0dd1fd2e3fc70f5eee72cc64eb29bbe77140adf
kayetavi <avijitkayet97@gmail.com>
2026-01-06 13:39:00 +0530

    Delete js/utils.js

--- js/utils.js
+++
@@ -1,5 +0,0 @@
-export function tempBand(temp) {
-  if (temp <= 120) return { band: "LOW", min: 0, max: 120 };
-  if (temp <= 260) return { band: "MEDIUM", min: 121, max: 260 };
-  return { band: "HIGH", min: 261, max: 1000 };
-}
