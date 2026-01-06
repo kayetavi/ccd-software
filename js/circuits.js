17c9f3bd92bc2ba72faa50ce8cb56aba798db653
kayetavi <avijitkayet97@gmail.com>
2026-01-06 09:28:19 +0530

    Update circuits.js

--- js/circuits.js
+++ js/circuits.js
@@ -1,12 +1,35 @@
 import { supabase } from './supabase.js';
 
+const loopId = localStorage.getItem("loop_id");
+const circuitList = document.getElementById("circuitList");
+
+async function loadCircuits() {
+  const { data } = await supabase
+    .from("circuits")
+    .select("*")
+    .eq("loop_id", loopId);
+
+  circuitList.innerHTML = "";
+  data.forEach(c => {
+    circuitList.innerHTML +=
+      `<li onclick="selectCircuit('${c.id}')">${c.circuit_name}</li>`;
+  });
+}
+
 window.saveCircuit = async function () {
   const name = document.getElementById("circuit").value;
   const material = document.getElementById("material").value;
 
   await supabase.from("circuits").insert([
-    { circuit_name: name, material: material }
+    { loop_id: loopId, circuit_name: name, material }
   ]);
 
-  alert("Circuit Added");
+  loadCircuits();
 };
+
+window.selectCircuit = function (id) {
+  localStorage.setItem("circuit_id", id);
+  location.href = "damage.html";
+};
+
+loadCircuits();
